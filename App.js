c47803bd3f767fc046eb724a43c4c18a8b6c3f88
Edoardo Vacca <edoardo.vacca@hotmail.it>
2024-01-21 23:28:33 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, Alert, TextInput } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const showAlert = () => {
+    Alert.alert("Hello", "You typed: " + message);
+  };
+
+  const [message, setMessage] = useState("");
+
+  /* const styles = StyleSheet.create({
+    container: {
+      backgroundColor: "grey",
+    },
+  });*/
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello!</Text>
+      <TextInput
+        style={{ width: 200, borderColor: "gray", borderWidth: 1 }}
+        onChangeText={(text) => setMessage(text)}
+      />
+      <Button title="Press me" onPress={showAlert} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
